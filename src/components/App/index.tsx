487d3abf158e1a011e697d45a9fcfd67e8ef7a21
Saad-Daoud <h.saad.daoud@gmail.com>
2021-09-19 18:13:45 +0100

    prêt au dev

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./App.scss";
+
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            Hello from App;
+        </div>
+    )
+} 
+
+export default App;
