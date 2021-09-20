5fdcdc56bd9ceb02a61b2d15a796b114b28f0086
Saad-Daoud <h.saad.daoud@gmail.com>
2021-09-20 14:47:53 +0100

    part1 30'

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import NumberDisplay from "../NumberDisplay";
 
 import "./App.scss";
+
+
 
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            Hello from App;
+            <div className="Header">
+                <NumberDisplay value={0} />
+                <NumberDisplay value={23} />
+            </div>
+            <div className="Body">Body</div>
         </div>
     )
 } 
